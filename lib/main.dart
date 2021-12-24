2716e653d0baf0d1004d8a427716fa8ac4ad873e
Airat Galyamov <galyamov.airat@gmail.com>
2021-12-24 11:22:46 +0500

    Виджет Expanded. Темы.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_demo/app_bar_screen.dart';
-import 'package:widgets_demo/navigation_drawer_screen.dart';
-import 'package:widgets_demo/switches_demo_screen.dart';
-import 'auth_screen.dart';
-import 'text_demo_screen.dart';
+import 'package:widgets_demo/screens/app_bar_screen.dart';
+import 'package:widgets_demo/screens/navigation_drawer_screen.dart';
+import 'package:widgets_demo/screens/switches_demo_screen.dart';
+import 'package:widgets_demo/screens/theme_demo_screen.dart';
+import 'screens/auth_screen.dart';
+import 'screens/expanded_demo_screen.dart';
+import 'screens/text_demo_screen.dart';
 
 void main() {
-  runApp(const AppBarScreen());
+  runApp(const ThemesDemoScreen());
 }
 
 
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -26,15 +26,18 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
             width: double.infinity,
-            child: Column(children: [
-              SizedBox(height: 60,),
-              const SizedBox(width: 110, height: 84, child: Image(image: AssetImage('assets/dart-logo.png')),),
-              SizedBox(height: 20,),
-              Text('Введите логин в виде 10 цифр номера телефона',
-                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-              SizedBox(height: 20,),
-              const SizedBox(width: 224,
-                child: TextField(
+            height: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                SizedBox(height: 150,),
+                const SizedBox(width: 110, height: 84, child: Image(image: AssetImage('assets/dart-logo.png')),),
+                SizedBox(height: 20,),
+                Text('Введите логин в виде 10 цифр номера телефона',
+                  style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
+                SizedBox(height: 20,),
+                TextField(
+                  keyboardType: TextInputType.phone,
                   decoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xFFeceff1),
@@ -43,10 +46,8 @@ class AuthScreen extends StatelessWidget {
                     labelText: 'Телефон',
                   ),
                 ),
-              ),
-              SizedBox(height: 20,),
-              const SizedBox(width: 224,
-                child: TextField(
+                SizedBox(height: 20,),
+                TextField(
                   obscureText: true,
                   decoration: InputDecoration(
                     filled: true,
@@ -56,26 +57,26 @@ class AuthScreen extends StatelessWidget {
                     labelText: 'Пароль',
                   ),
                 ),
-              ),
-              SizedBox(height: 28,),
-              SizedBox(width: 154, height: 42, child:
-              ElevatedButton(onPressed: () {},
-                child: Text('Войти'),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF0079D0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(36.0),
+                SizedBox(height: 28,),
+                SizedBox(width: 154, height: 42, child:
+                ElevatedButton(onPressed: () {},
+                  child: Text('Войти'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF0079D0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(36.0),
+                    ),
                   ),
+                )
                 ),
-              )
-              ),
-              SizedBox(height: 32,),
-              InkWell(child: const Text('Регистрация', style: linkTextStyle,),
-                  onTap: () {}),
-              SizedBox(height: 20,),
-              InkWell(child: Text('Забыли пароль?', style: linkTextStyle,), onTap: () {})
+                SizedBox(height: 32,),
+                InkWell(child: const Text('Регистрация', style: linkTextStyle,),
+                    onTap: () {}),
+                SizedBox(height: 20,),
+                InkWell(child: Text('Забыли пароль?', style: linkTextStyle,), onTap: () {})
 
-            ],),
+              ],),
+            ),
           )
       ),
     );
