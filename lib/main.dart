38db374e76249bf5967d0af81ee6972dc59e7fc4
Airat Galyamov <galyamov.airat@gmail.com>
2022-01-02 23:36:57 +0500

    Использование Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_demo/screens/app_bar_screen.dart';
+import 'package:widgets_demo/screens/google_fonts_screen.dart';
 import 'package:widgets_demo/screens/navigation_demo.dart';
 import 'package:widgets_demo/screens/navigation_drawer_screen.dart';
 import 'package:widgets_demo/screens/networking_screen.dart';
@@ -13,7 +14,7 @@ import 'screens/shared_pref_screen.dart';
 import 'screens/text_demo_screen.dart';
 
 void main() {
-  runApp(const FilesDemoScreen());
+  runApp(const GoogleFontsDemoScreen());
 }
 
 
